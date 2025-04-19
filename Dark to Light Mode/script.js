739513095ecd2b423362e691cf94cc92ff7264a7
RSowmiya-3272 <sowmiyauk10@gmail.com>
2025-04-19 09:10:43 +0530

    Add files via upload

---
+++ Dark to Light Mode/script.js
@@ -0,0 +1,12 @@
+function input(){
+    var textbox=document.getElementById("cont");
+    textbox.style.color="white";
+    textbox.style.backgroundColor="black";
+    textbox.style.height="1000px";
+}
+function inputFeild(){
+    var textbox=document.getElementById("cont");
+    textbox.style.color="black";
+    textbox.style.backgroundColor="white";
+    textbox.style.height="1000px";
+}
